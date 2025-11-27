354acbe3e7cc2ad8e1d5b1e8941bd6e961a0a3cc
Your Name <your-email@example.com>
2025-11-27 19:46:55 +0530

    Update: Use new my-portfolio-key SSH key pair

--- Terraform/variables.tf
+++ Terraform/variables.tf
@@ -26,7 +26,7 @@ variable "instance_name" {
 variable "key_name" {
   description = "Existing key pair name in AWS"
   type        = string
-  default = "my-portfolio-key-2"
+  default = "my-portfolio-key"
 }
 
 variable "vpc_cidr" {
